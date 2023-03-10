9439298cf627bf2e3cf4f5ef34f683ad06fa2715
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-10 20:27:38 +0000

    link create  profile page to home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import CreateYourProfilePage from "./components/CreateProfile/CreateYourProfilePage";
 
 // import About from "./pages/About";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 
 const App = () => (
 	<div>
-		<CreateYourProfilePage />
+
+	<Routes>
+	<Route path="/" element={<Home />} />
+	<Route path="/createprofile" element={<CreateYourProfilePage />} />
+	</Routes>
 	</div>
-	// <Routes>
-	// 	<Route path="/" element={<Home />} />
-	// 	<Route path="/about/this/site" element={<About />} />
-	// 	<CreateYourProfilePage />
-	// </Routes>
 );
 
 export default App;
