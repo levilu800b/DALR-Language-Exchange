23d8f80777d0665d969371a6ef66ee930de8ee35
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-11 14:40:45 +0000

    add navbar as global

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
+
 import { Route, Routes } from "react-router-dom";
 import CreateYourProfilePage from "./components/CreateProfile/CreateYourProfilePage";
 
 // import About from "./pages/About";
 import Home from "./pages/Home";
+import NavBar from "./components/HomePage/NavBar/NavBar";
 
 const App = () => (
 	<div>
-
+		<NavBar />
 	<Routes>
 	<Route path="/" element={<Home />} />
 	<Route path="/createprofile" element={<CreateYourProfilePage />} />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 import Hero from "../components/HomePage/Hero/Hero";
-import NavBar from "../components/HomePage/NavBar/NavBar";
+// import NavBar from "../components/HomePage/NavBar/NavBar";
 import "./Home.css";
 export function Home() {
 
 	return (
 		<>
-		<NavBar />
+		{/* <NavBar /> */}
 		<Hero />
 		</>
 	);
