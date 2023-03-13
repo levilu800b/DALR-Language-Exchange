1413d4763e4fb7f3a7137626708a1ae84ab5c08d
levilu800b <levilu800b@gmail.com>
2023-03-13 20:16:19 +0000

    fixing bugs

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
-// import "antd/dist/reset.css";
-// import "../assets/css/general.css";
+import "antd/dist/reset.css";
+import "./assets/css/general.css";
 import { Layout } from "antd";
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "../SideBar/SideBar";
-import ContactForm from "../components/users/AddUsers";
-import UserList from "../components/users/ListUsers";
+import Hedear from "./components/generic/Header";
+import Footer from "./components/generic/Footer";
+import Sidebar from "./components/generic/Sidebar";
+import DashboardLogin from "./components/generic/DashboardLogin";
+import DashboardSignup from "./components/generic/DashboardSignup";
+import ContactForm from "./components/users/AddUsers";
+import UserList from "./components/users/ListUsers";
 
 const { Content } = Layout;
 
-function Dashboard() {
+function App() {
 	const [users, setUsers] = useState([]);
 
 	useEffect(() => {
@@ -18,33 +22,37 @@ function Dashboard() {
 			.then((data) => setUsers(data));
 	}, []);
 
-	const AddUser = (video, id) => {
+	const AddUsers = (video, id) => {
 		video.id = id;
 		setUsers([...users, video]);
 	};
-
+	// console.log(users);
 	return (
-		<Layout>
+		<div className="App">
 			<Layout>
-				<Sidebar />
-				<Content className="content">
-					<Routes>
-						{/* <Route path="/login" element={<DashboardLogin />} />
-						<Route path="/signup" element={<DashboardSignup />} /> */}
-						<Route
-							path="/adduser/addUser"
-							element={<ContactForm AddUsers={AddUser} />}
-						/>
+				<Hedear />
+				<Layout>
+					<Sidebar />
+					<Content className="content">
+						<Routes>
+							<Route path="/login" element={<DashboardLogin />} />
+							<Route path="/signup" element={<DashboardSignup />} />
+							<Route
+								path="/users/addUsers"
+								element={<ContactForm AddUsers={AddUsers} />}
+							/>
 
-						<Route
-							path="/users/listUsers"
-							element={<UserList users={users} />}
-						/>
-					</Routes>
-				</Content>
+							<Route
+								path="/users/listUsers"
+								element={<UserList users={users} />}
+							/>
+						</Routes>
+					</Content>
+				</Layout>
+				<Footer />
 			</Layout>
-		</Layout>
+		</div>
 	);
 }
 
-export default Dashboard;
+export default App;
