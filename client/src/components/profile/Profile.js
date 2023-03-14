a2429bc258076dfad6b5abbc91a89fc603c662ad
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-14 23:22:32 +0000

    add header section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,22 +1,33 @@
 import React from "react";
 import "./ProfileStyle.css";
 import profile from "../../assets/profile.jpg";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
-    <div>
+    <div className="content">
     <header className="profile-header">
       <img src={profile} alt="Profile picture" />
+      <br />
     <div className="profile-info">
-      <h1>Ahmed Mohamed</h1>
-      <h2>Software Engineer</h2>
+      <h2>Ahmed Mohamed</h2>
+      <br />
+      <h3>Manchester , UK</h3>
+      <br />
+      <div className="bt-profile">
+      <Link to="/" className="btn-light">Follow</Link>
+      <Link to="/" className="btn-light">Chat</Link>
+
+      </div>
     </div>
     </header>
-    <main>
+    <main className="main-profile">
       {/* Add main content here */}
+      <h1>maaaaai</h1>
     </main>
     <footer>
       {/* Add footer content here */}
+      <h1>fooo</h1>
     </footer>
   </div>
   );
