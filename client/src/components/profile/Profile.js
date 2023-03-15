99f9d11305c3365599c602d26f6b30bd67c92f07
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-15 14:45:32 +0000

    fix-line-error

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 import { Descriptions } from "antd";
 const Profile = () => {
   return (
-    <div className="content">
+    <div className="profile-content">
     <header className="profile-header">
-      <img src={profile} alt="Profile picture" />
+      <img src={profile} alt="Profile picture"  aria-hidden />
       <br />
     <div className="profile-info">
       <h2>Ahmed Mohamed</h2>
@@ -17,14 +17,13 @@ const Profile = () => {
       <div className="bt-profile">
       <Link to="/" className="btn-light">Follow</Link>
       <Link to="/" className="btn-light">Chat</Link>
-
       </div>
     </div>
     </header>
     <main className="main-profile">
     <Descriptions title="User Info">
     <Descriptions.Item label="UserName">Ahmed Mohamed</Descriptions.Item>
-    <Descriptions.Item label="Email"><a href="#" style={{ color:"black" }}>Ahmed@gmail.com</a></Descriptions.Item>
+    <Descriptions.Item label="Email"><a href="#email" style={{ color:"black" }}>Ahmed@gmail.com</a></Descriptions.Item>
     <Descriptions.Item label="Language Speak">Arabic</Descriptions.Item>
     <Descriptions.Item label="Language Interest">English</Descriptions.Item>
     <Descriptions.Item label="city">Manchester</Descriptions.Item>
