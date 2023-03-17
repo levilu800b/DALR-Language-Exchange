841df2824aca443182c4fedc07ca985957639f45
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-17 20:14:25 +0000

    fix error

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,38 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import profile from "../../assets/bg1.jpg";
+
 import "./ProfileStyle.css";
-import profile from "../../assets/profile.jpg";
+
 import { Link } from "react-router-dom";
 import { Descriptions } from "antd";
-const Profile = () => {
-  return (
-    <div className="profile-content">
-    <header className="profile-header">
-      <img src={profile} alt="Profile picture"  aria-hidden />
-      <br />
-    <div className="profile-info">
-      <h2>Ahmed Mohamed</h2>
-      <br />
-      <h3>Manchester , UK</h3>
-      <br />
-      <div className="bt-profile">
-      <Link to="/" className="btn-light">Follow</Link>
-      <Link to="/" className="btn-light">Chat</Link>
-      </div>
-    </div>
-    </header>
-    <main className="main-profile">
-    <Descriptions title="User Info">
-    <Descriptions.Item label="UserName">Ahmed Mohamed</Descriptions.Item>
-    <Descriptions.Item label="Email"><a href="#email" style={{ color:"black" }}>Ahmed@gmail.com</a></Descriptions.Item>
-    <Descriptions.Item label="Language Speak">Arabic</Descriptions.Item>
-    <Descriptions.Item label="Language Interest">English</Descriptions.Item>
-    <Descriptions.Item label="city">Manchester</Descriptions.Item>
-    <Descriptions.Item label="Country">UK</Descriptions.Item>
+const Profile = ({ setAuth }) => {
+	const [data, setData] = useState("");
 
-  </Descriptions>
-    </main>
-  </div>
-  );
+	const getProfile = async () => {
+		try {
+			const res = await fetch("api/dashboard/", {
+				method: "GET",
+				headers: { token: localStorage.token },
+			});
+
+			const parseData = await res.json();
+			setData(parseData);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	const logout = async (e) => {
+		e.preventDefault();
+		try {
+			localStorage.removeItem("token");
+			setAuth(false);
+			toast.success("Logout successfully");
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	useEffect(() => {
+		getProfile();
+	}, []);
+	console.log(data);
+
+	return (
+		<>
+			<div className="profile-content">
+				<header className="profile-header">
+					<img src={profile} alt="Profile picture" aria-hidden />
+					<br />
+					<div className="profile-info">
+						<h2>
+							{data.user_firstname} {data.user_secondname}
+						</h2>
+						<br />
+						<h3>
+							{data.user_city}, {data.user_country}
+						</h3>
+						<br />
+						<div className="bt-profile">
+							<Link to="/" className="btn-light">
+								Follow
+							</Link>
+							<Link to="/" className="btn-light">
+								Chat
+							</Link>
+						</div>
+					</div>
+				</header>
+				<main className="main-profile">
+					<Descriptions title="User Info">
+						<Descriptions.Item label="UserName">
+							{data.user_firstname} {data.user_secondname}
+						</Descriptions.Item>
+						<Descriptions.Item label="Email">
+							<a href="#email" style={{ color: "black" }}>
+								{data.user_email}
+							</a>
+						</Descriptions.Item>
+						<Descriptions.Item label="Language Speak">
+							{data.user_language_speak}
+						</Descriptions.Item>
+						<Descriptions.Item label="Language Interest">
+							{data.user_language_interest}
+						</Descriptions.Item>
+						<Descriptions.Item label="city">
+							{" "}
+							{data.user_city}{" "}
+						</Descriptions.Item>
+						<Descriptions.Item label="Country">
+							{data.user_country}{" "}
+						</Descriptions.Item>
+					</Descriptions>
+				</main>
+				<button
+					onClick={(e) => logout(e)}
+					className="mt-5 btn btn-primary text-buttom bg-info"
+				>
+					Logout
+				</button>
+			</div>
+		</>
+	);
 };
-
 export default Profile;
