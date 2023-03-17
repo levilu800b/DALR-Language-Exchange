0599f7bb1c44504d1e431bb0d12c9ecfb38c51f3
rebwar <rebwar1.azizi@gmail.com>
2023-03-17 08:37:20 +0000

    add pur rquest to backEnd

--- server/routerAuth/dashboard.js
+++ server/routerAuth/dashboard.js
@@ -31,4 +31,52 @@ router.get("/all", authorize, async (req, res) => {
 	}
 });
 
+router.put("/update/:userId", async (req, res) => {
+	const { userId } = req.params;
+	const {
+		user_firstname,
+		user_secondname,
+		user_email,
+		user_language_speak,
+		user_language_interest,
+		user_city,
+		user_country,
+	} = req.body;
+
+	try {
+		const updateUserQuery = `
+		UPDATE user_profiles
+		SET user_firstname=$1, user_secondname=$2, user_email=$3,
+			user_language_speak=$4, user_language_interest=$5,
+			user_city=$6, user_country=$7
+		WHERE user_id=$8
+		RETURNING *;
+	  `;
+		const values = [
+			user_firstname,
+			user_secondname,
+			user_email,
+			user_language_speak,
+			user_language_interest,
+			user_city,
+			user_country,
+			userId,
+		];
+
+		const { rows } = await db.query(updateUserQuery, values);
+
+		res.status(200).json({
+			success: true,
+			message: `User with ID ${userId} updated successfully.`,
+			data: rows[0],
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			success: false,
+			message: `Could not update user with ID ${userId}.`,
+		});
+	}
+});
+
 module.exports = router;
