4d6d526de2d24edbda108d19eac715f642e196e3
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-20 21:39:13 +0000

    add on change function

--- client/src/components/EditProfile/EditProfile.js
+++ client/src/components/EditProfile/EditProfile.js
@@ -4,6 +4,29 @@ import { toast } from "react-toastify";
 import "./EditProfile.css";
 
 const EditProfile = () => {
+	const [inputs, setInputs] = useState({
+		firstname: "",
+		secondname: "",
+		email: "",
+		password: "",
+		language_speak: "",
+		language_interest: "",
+		city: "",
+		country: "",
+	});
+	const {
+		firstname,
+		secondname,
+		email,
+		password,
+		language_speak,
+		language_interest,
+		city,
+		country,
+	} = inputs;
+	const onChange = (e) =>
+		setInputs({ ...inputs, [e.target.name]: e.target.value });
+
 	return <div>Edit Profile</div>;
 };
 
