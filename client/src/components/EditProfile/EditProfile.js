a17e2bde20446a2ea82184b1c2fad5b5dcf6d993
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-20 21:37:00 +0000

    add useState function

--- client/src/components/EditProfile/EditProfile.js
+++ client/src/components/EditProfile/EditProfile.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import "./EditProfile.css";
 
 const EditProfile = () => {
