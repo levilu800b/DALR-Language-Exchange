a7a1058695e1d9c050172b664b0bd194c476a828
rebwar <rebwar1.azizi@gmail.com>
2023-03-20 23:01:22 +0000

    fix error for router

--- client/src/components/massages/Massage.js
+++ client/src/components/massages/Massage.js
@@ -31,129 +31,120 @@
 // 	);
 // }
 //🌧
-import React from "react";
-import { Menu } from "antd";
-import { Layout } from "antd";
-import { Link, Routes, Route } from "react-router-dom";
-import Friends from "./Users/Friends";
 
-const { Header, Footer, Content } = Layout;
+//🍉🍉🍉🍉🍉🍉🍉🍉
+import React, { useState } from "react";
+import { Menu, Layout } from "antd";
+import { Link, Routes, Route, useParams, useNavigate } from "react-router-dom";
+import Friends from "./Users/Friends";
+import Exam from "./Users/Exam";
 import imfg from "../../assets/bg1.jpg";
 import "./massage.css";
 import "./navbar.css";
 
+const { Header, Footer, Content } = Layout;
+
 export default function Massage({ data, selectedUser }) {
-	console.log("selectedUser", selectedUser);
-	const user = data.find((u) => u.user_id === selectedUser.user_id);
+	const [showExam, setShowExam] = useState(false);
+	const { userId } = useParams();
+	const user = data.find((u) => u.user_id === parseInt(userId));
+
+	const navigate = useNavigate();
+
+	const handleFriendClick = () => {
+		setShowExam(true);
+		navigate("/friends");
+	};
+
+	const handleProfileClick = () => {
+		setShowExam(true);
+		navigate("/profile");
+	};
+
+	const handleMessageClick = () => {
+		setShowExam(false);
+		navigate("/message");
+	};
+
+	const handleCorrectionsClick = () => {
+		setShowExam(false);
+		navigate("/corrections");
+	};
 
 	return (
 		<>
 			<div>
-				<h2 className="title">Exchange with {selectedUser.user_firstname} </h2>
+				<h2 className="title">Exchange with {selectedUser.user_firstname}</h2>
 			</div>
 			<Layout>
-				<Header className="headerMessage" style={{ backgroundColor: "#fff" }}>
-					<Menu mode={"horizontal"} theme={"light"}>
-						<Menu.Item
-							className="menu-item-Message"
-							key={"friends"}
-							style={{ width: "25%" }}
-						>
-							<Link to="#" className="a">
+				<Header className="headerMessage">
+					<Menu mode="horizontal" theme="light">
+						<Menu.Item className="menu-item-Message" key="friends">
+							<Link to="#" className="a" onClick={handleFriendClick}>
 								Friends
 							</Link>
 						</Menu.Item>
-						<Menu.Item
-							className="menu-item-Message"
-							key={"profile"}
-							style={{ width: "25%" }}
-						>
-							<Link to="#" className="a">
+						<Menu.Item className="menu-item-Message" key="profile">
+							<Link to="#" className="a" onClick={handleProfileClick}>
 								Profile
 							</Link>
 						</Menu.Item>
-						<Menu.Item
-							className="menu-item-Message"
-							key={"message"}
-							style={{ width: "25%" }}
-						>
-							<Link to="#" className="a">
+						<Menu.Item className="menu-item-Message" key="message">
+							<Link to="#" className="a" onClick={handleMessageClick}>
 								Message
 							</Link>
 						</Menu.Item>
-						<Menu.Item
-							className="menu-item-Message"
-							key={"corrections"}
-							style={{ width: "25%" }}
-						>
-							<Link to="#" className="a">
+						<Menu.Item className="menu-item-Message" key="corrections">
+							<Link to="#" className="a" onClick={handleCorrectionsClick}>
 								Corrections
 							</Link>
 						</Menu.Item>
 					</Menu>
 				</Header>
 				<Content className="contentMessage">
-					{/* <Routes>
-						<Route path="/friends" element={<Friends />} />
-					</Routes> */}
-					<section className="recipe_all">
-						<div className="overall_ditail">
-							<div className="img">
-								<img src={imfg} alt="" width="150px" height="200px" />
-							</div>
+					{showExam ? (
+						<Friends />
+					) : (
+						<section className="recipe_all">
+							<div className="overall_ditail">
+								<div className="img">
+									<img src={imfg} alt="" width="150px" height="200px" />
+								</div>
 
-							<div className="text">
-								<div>
-									<p>
-										<span>First name:</span>
-									</p>
-									<p>
-										<span>Last name:</span>
-									</p>
-									<p>
-										<span>City:</span>
-									</p>
-									<p>
-										<span>Native country:</span>
-									</p>
-									<p>
-										<span>Native language:</span>
-									</p>
-									<p>
-										<span>Studied language:</span>
-									</p>
-								</div>
-								<div>
-									<p>{selectedUser.user_firstname}</p>
-									<p>{selectedUser.user_secondname}</p>
-									<p>{selectedUser.user_city}</p>
-									<p>{selectedUser.user_country}</p>
-									<p>{selectedUser.user_language_speak}</p>
-									<p>{selectedUser.user_language_interest}</p>
+								<div className="text">
+									<div>
+										<p>
+											<span>First name:</span>
+										</p>
+										<p>
+											<span>Last name:</span>
+										</p>
+										<p>
+											<span>City:</span>
+										</p>
+										<p>
+											<span>Native country:</span>
+										</p>
+										<p>
+											<span>Native language:</span>
+										</p>
+										<p>
+											<span>Studied language:</span>
+										</p>
+									</div>
+									<div>
+										<p>{selectedUser.user_firstname}</p>
+										<p>{selectedUser.user_secondname}</p>
+										<p>{selectedUser.user_city}</p>
+										<p>{selectedUser.user_country}</p>
+										<p>{selectedUser.user_language_speak}</p>
+										<p>{selectedUser.user_language_interest}</p>
+									</div>
 								</div>
 							</div>
-						</div>
-					</section>
-					<div class="icon">
-						<h3>Description</h3>
-						<p>
-							There's nobody to talk to in my village, so maybe with a bit of
-							luck I'll meet some talkative people here. I'm into languages,
-							travelling, and meaningful or meaningless conversations,
-							preferably in English, but I'll gladly help with Polish. Also I'll
-							gladly pick up some words in Italian, French and Arabic.
-						</p>
-					</div>
-					<div class="icon">
-						<h3>New message</h3>
-						<textarea
-							name="message"
-							id="message"
-							cols="30"
-							rows="10"
-						></textarea>
-						<button>Send</button>
-					</div>
+							<Exam />
+						</section>
+					)}
 				</Content>
 				<Footer className="footerMessage">Footer</Footer>
 			</Layout>
