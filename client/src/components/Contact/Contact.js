d22f24341ae408139482efc83585148978cb0cb9
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-03-21 20:51:12 +0000

    add the form

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Button, Form, Input, InputNumber } from "antd";
+import "./Contact.css";
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+/* eslint-enable no-template-curly-in-string */
+
+const onFinish = (values, any) => {
+  console.log(values);
+};
 const Contact = () => {
   return (
-    <div>Contact</div>
-  );
+    <div className="cont">
+      <h1 style={{ color :"black" }}>contact</h1>
+  <Form
+    {...layout}
+    name="nest-messages"
+    onFinish={onFinish}
+    style={{ maxWidth: 600 }}
+    validateMessages={validateMessages}
+  >
+
+  </Form>
+  </div>
+);
 };
 
 export default Contact;
