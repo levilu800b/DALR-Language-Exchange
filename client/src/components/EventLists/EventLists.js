8bc21c8d9a680ced80d2e867ee3cffe8fac7a92a
clav00cl <clav00cl@hotmail.com>
2023-03-23 21:33:33 +0000

    list of events

--- client/src/components/EventLists/EventLists.js
+++ client/src/components/EventLists/EventLists.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { List } from "antd";
 import "./EventLists.css";
+import img from "../../assets/faceImoje.png";
 
-const EventLists = () => {
-  return (
-    <div>EventLists</div>
-  );
+const EventsList = () => {
+ const [events, setEvents] = useState([]);
+
+ useEffect(() => {
+  const fetchEvents = async () => {
+   try {
+    const response = await fetch("/api/dashboard/events");
+    const data = await response.json();
+    setEvents(data);
+   } catch (error) {
+    console.error(error.message);
+   }
+  };
+  fetchEvents();
+ }, []);
+
+ return (
+  <div className="event_conte">
+   <List
+    itemLayout="vertical"
+    dataSource={events}
+    renderItem={(event) => (
+     <section className="container_all-message" key={event.id} >
+      <div className="overall_ditail-message">
+       <div>
+        <img className="img-message" src={img} alt="" />
+       </div>
+
+       <div className="text-message">
+        <div className="Native">
+         <p>
+          <span>Title:</span> {event.title}
+         </p>
+         <p>
+          <span>Description:</span> {event.description}
+         </p>
+         <p>
+          <span>Datetime:</span> {event.datetime}
+         </p>
+        </div>
+        <div>
+         <p>
+          <span>Languages:</span> {event.languages}
+         </p>
+         <p>
+          <span>Location:</span> {event.location}
+         </p>
+         <p>
+          <span>Link:</span> {event.link}
+         </p>
+        </div>
+       </div>
+      </div>
+     </section>
+    )}
+   />
+  </div>
+ );
 };
 
-export default EventLists;
+export default EventsList;
