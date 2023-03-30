cd91fd2fb380f9c32097f35a414241a15375c791
levilu800b <levilu800b@gmail.com>
2023-03-30 21:06:44 +0100

    error fix
    Co-Authored-By: Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
    Co-Authored-By: clav00cl <105980474+clav00cl@users.noreply.github.com>
    Co-Authored-By: rebwar <anvar.azizi21@gmail.com>

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,14 +1,77 @@
+// import React, { useEffect, useState } from "react";
+// import profile from "../../assets/bg2.jpg";
+// import EditProfile from "./EditProfile";
+
+// import "./ProfileStyle.css";
+
+// import { Link } from "react-router-dom";
+// import { Descriptions } from "antd";
+
+// const Profile = () => {
+// 	const [data, setData] = useState("");
+
+// 	const getProfile = async () => {
+// 		try {
+// 			const res = await fetch("api/dashboard/", {
+// 				method: "GET",
+// 				headers: { token: localStorage.token },
+// 			});
+
+// 			const parseData = await res.json();
+// 			setData(parseData);
+// 		} catch (err) {
+// 			console.error(err.message);
+// 		}
+// 	};
+
+// 	useEffect(() => {
+// 		getProfile();
+// 	}, []);
+
+// 	return (
+// 		<>
+// 			<div className="profile-content">
+// 				<header className="profile-header">
+// 					<img src={profile} alt="Profile picture" aria-hidden />
+// 					<br />
+// 					<div className="profile-info">
+// 						<h2>
+// 							{data.user_firstname} {data.user_secondname}
+// 						</h2>
+// 						<Descriptions bordered>
+// 							<Descriptions.Item label="Email">{data.user_email}</Descriptions.Item>
+// 							<Descriptions.Item label="City">{data.user_city}</Descriptions.Item>
+// 							<Descriptions.Item label="Country">{data.user_country}</Descriptions.Item>
+// 							<Descriptions.Item label="Language You Speak">{data.user_language_speak}</Descriptions.Item>
+// 							<Descriptions.Item label="Language You're Interested In">{data.user_language_interest}</Descriptions.Item>
+// 						</Descriptions>
+// 						<br />
+// 						<Link to="/change-password" className="profile-link">
+// 							Change Password
+// 						</Link>
+// 					</div>
+// 				</header>
+
+// 				<EditProfile user={data} setUser={setData} />
+// 			</div>
+// 		</>
+// 	);
+// };
+
+// export default Profile;
+//🌧🍉✅
 import React, { useEffect, useState } from "react";
 import profile from "../../assets/bg2.jpg";
 import EditProfile from "./EditProfile";
 import "./ProfileStyle.css";
+
+//import { Link } from "react-router-dom";
 import { Descriptions, Button, Modal } from "antd";
 
-
 const Profile = () => {
+	const [data, setData] = useState("");
+	const [isModalVisible, setIsModalVisible] = useState(false);
 
-const [data, setData] = useState("");
- const [isModalVisible, setIsModalVisible] = useState(false);
 
  const getProfile = async () => {
   try {
@@ -45,6 +108,23 @@ const [data, setData] = useState("");
 						<h2>
 							{data.user_firstname} {data.user_secondname}
 						</h2>
+						<Descriptions bordered>
+							<Descriptions.Item label="Email">
+								{data.user_email}
+							</Descriptions.Item>
+							<Descriptions.Item label="City">
+								{data.user_city}
+							</Descriptions.Item>
+							<Descriptions.Item label="Country">
+								{data.user_country}
+							</Descriptions.Item>
+							<Descriptions.Item label="Language You Speak">
+								{data.user_language_speak}
+							</Descriptions.Item>
+							<Descriptions.Item label="Language You're Interested In">
+								{data.user_language_interest}
+							</Descriptions.Item>
+						</Descriptions>
 						<br />
 						<h3 className="bt-profile">
 							{data.user_city}, {data.user_country}
@@ -85,10 +165,85 @@ const [data, setData] = useState("");
 <Modal title="Edit Profile" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
      <EditProfile user={data} setUser={setData} handleOk={handleOk} />
     </Modal>
-    </main>
+</main>
 		</div>
 		</>
 	);
 };
 
 export default Profile;
+
+
+
+// import React, { useEffect, useState } from "react";
+// import profile from "../../assets/bg2.jpg";
+// import EditProfile from "./EditProfile";
+
+// import "./ProfileStyle.css";
+
+// import { Link } from "react-router-dom";
+// import { Descriptions, Button, Modal } from "antd";
+
+// const Profile = () => {
+//  const [data, setData] = useState("");
+//  const [isModalVisible, setIsModalVisible] = useState(false);
+
+//  const getProfile = async () => {
+//   try {
+//    const res = await fetch("api/dashboard/", {
+//     method: "GET",
+//     headers: { token: localStorage.token },
+//    });
+
+//    const parseData = await res.json();
+//    setData(parseData);
+//   } catch (err) {
+//    console.error(err.message);
+//   }
+//  };
+
+//  useEffect(() => {
+//   getProfile();
+//  }, []);
+
+//  const handleOk = () => {
+//   setIsModalVisible(false);
+//  };
+
+//  const handleCancel = () => {
+//   setIsModalVisible(false);
+//  };
+
+//  return (
+//   <>
+//    <div className="profile-content">
+//     <header className="profile-header">
+//      <img src={profile} alt="Profile picture" aria-hidden />
+//      <br />
+//      <div className="profile-info">
+//       <h2>
+//        {data.user_firstname} {data.user_secondname}
+//       </h2>
+//       <Descriptions bordered>
+//        <Descriptions.Item label="Email">{data.user_email}</Descriptions.Item>
+//        <Descriptions.Item label="City">{data.user_city}</Descriptions.Item>
+//        <Descriptions.Item label="Country">{data.user_country}</Descriptions.Item>
+//        <Descriptions.Item label="Language You Speak">{data.user_language_speak}</Descriptions.Item>
+//        <Descriptions.Item label="Language You're Interested In">{data.user_language_interest}</Descriptions.Item>
+//       </Descriptions>
+//       <br />
+//       <Button type="primary" onClick={() => setIsModalVisible(true)}>
+//        Edit Profile
+//       </Button>
+//      </div>
+//     </header>
+
+//     <Modal title="Edit Profile" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
+//      <EditProfile user={data} setUser={setData} handleOk={handleOk} />
+//     </Modal>
+//    </div>
+//   </>
+//  );
+// };
+
+// export default Profile;
