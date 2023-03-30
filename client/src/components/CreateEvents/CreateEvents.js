9a86927175c92112e09c16b46817266b79d8d23f
rebwar <rebwar1.azizi@gmail.com>
2023-03-30 13:39:50 +0100

    fix craete event for front end that pass email of log in usr to bak end

--- client/src/components/CreateEvents/CreateEvents.js
+++ client/src/components/CreateEvents/CreateEvents.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./CreateEvents.css";
 
 const CreateEvents = () => {
+	const [sender, setSender] = useState("");
+
+	const getProfile = async () => {
+		try {
+			const res = await fetch("api/dashboard/", {
+				method: "GET",
+				headers: { token: localStorage.token },
+			});
+
+			const parseData = await res.json();
+			setSender(parseData);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	useEffect(() => {
+		getProfile();
+	}, []);
+
 	const [formData, setFormData] = useState({
 		languages: "",
 		location: "",
@@ -10,6 +30,8 @@ const CreateEvents = () => {
 		title: "",
 		description: "",
 		datetime: "",
+		senderEmail: "",
+		senderId: "",
 	});
 
 	const [formErrors, setFormErrors] = useState({});
@@ -72,26 +94,29 @@ const CreateEvents = () => {
 			setFormErrors(errors);
 		} else {
 			try {
-				const result = await axios.post("api/dashboard/create_events", {
+				await axios.post("api/dashboard/create_events", {
 					languages,
 					location,
 					link,
 					title,
 					description,
 					datetime,
+					senderEmail: sender.user_email,
+					senderId: sender.user_id,
 				});
 
-				if (result.status === 201) {
-					setFormSuccess(true);
-					setFormData({
-						languages: "",
-						location: "",
-						link: "",
-						title: "",
-						description: "",
-						datetime: "",
-					});
-				}
+				setFormSuccess(true);
+
+				setFormData({
+					languages: "",
+					location: "",
+					link: "",
+					title: "",
+					description: "",
+					datetime: "",
+					senderEmail: "",
+					senderId: "",
+				});
 			} catch (error) {
 				console.error(error);
 				setFormErrors({ submit: "Something went wrong, please try again." });
@@ -109,106 +134,92 @@ const CreateEvents = () => {
 			title: "",
 			description: "",
 			datetime: "",
+			senderEmail: "",
+			senderId: "",
 		});
-
-		if (formErrors.cancel) {
-			setFormErrors({ ...formErrors, cancel: "" });
-		}
+		setFormErrors({});
 	};
+
 	return (
-		<div className="form_cont">
-			<form onSubmit={handleSubmit} className="cont-form">
-				<div className="one">
-					<div className="full-width">
-						<label htmlFor="languages">Language(s):</label>
-						<input
-							type="text"
-							id="languages"
-							name="languages"
-							value={formData.languages}
-							onChange={handleInputChange}
-						/>
-						{formErrors.languages && (
-							<span className="span_rec">{formErrors.languages}</span>
-						)}
-					</div>
-					<div className="full-width l">
-						<label htmlFor="location">Location:</label>
-						<input
-							type="text"
-							id="location"
-							name="location"
-							value={formData.location}
-							onChange={handleInputChange}
-						/>
-						{formErrors.location && (
-							<span className="span_rec">{formErrors.location}</span>
-						)}
-					</div>
-				</div>
-				<div className="one">
-					<div className="full-width">
-						<label htmlFor="link">Link for Zoom (if online):</label>
-						<input
-							type="text"
-							id="link"
-							name="link"
-							value={formData.link}
-							onChange={handleInputChange}
-						/>
-						{formErrors.link && (
-							<span className="span_rec">{formErrors.link}</span>
-						)}
-					</div>
-					<div className="full-width">
-						<label htmlFor="title">Title:</label>
-						<input
-							type="text"
-							id="title"
-							name="title"
-							value={formData.title}
-							onChange={handleInputChange}
-						/>
-						{formErrors.title && (
-							<span className="span_rec">{formErrors.title}</span>
-						)}
-					</div>
-				</div>
-				<div className="full-width">
-					<label htmlFor="description">Description:</label>
-					<textarea
-						id="description"
-						name="description"
-						value={formData.description}
-						onChange={handleInputChange}
-					/>
-					{formErrors.description && (
-						<span className="span_rec">{formErrors.description}</span>
-					)}
-				</div>
-				<div className="full-width">
-					<label htmlFor="datetime">Date & Time:</label>
-					<input
-						type="datetime-local"
-						id="datetime"
-						name="datetime"
-						value={formData.datetime}
-						onChange={handleInputChange}
-					/>
-					{formErrors.datetime && (
-						<span className="span_rec">{formErrors.datetime}</span>
-					)}
-				</div>
-				<div>
-					<button type="submit" className="btn-light">
-						Submit
-					</button>
-					{formErrors.submit && (
-						<span className="span_rec">{formErrors.submit}</span>
-					)}
-					<button type="cancel" onClick={handleCancel} className="btn-light">
-						Cancel
-					</button>
+		<div className="create-events-container">
+			<h2>Create Event</h2>
+			<form className="create-events-form" onSubmit={handleSubmit}>
+				<label htmlFor="languages">Languages *</label>
+				<input
+					type="text"
+					id="languages"
+					name="languages"
+					value={formData.languages}
+					onChange={handleInputChange}
+				/>
+				{formErrors.languages && (
+					<span className="error">{formErrors.languages}</span>
+				)}
+				<label htmlFor="location">Location *</label>
+				<input
+					type="text"
+					id="location"
+					name="location"
+					value={formData.location}
+					onChange={handleInputChange}
+				/>
+				{formErrors.location && (
+					<span className="error">{formErrors.location}</span>
+				)}
+
+				<label htmlFor="link">Link *</label>
+				<input
+					type="text"
+					id="link"
+					name="link"
+					value={formData.link}
+					onChange={handleInputChange}
+				/>
+				{formErrors.link && <span className="error">{formErrors.link}</span>}
+
+				<label htmlFor="title">Title *</label>
+				<input
+					type="text"
+					id="title"
+					name="title"
+					value={formData.title}
+					onChange={handleInputChange}
+				/>
+				{formErrors.title && <span className="error">{formErrors.title}</span>}
+
+				<label htmlFor="description">Description *</label>
+				<textarea
+					id="description"
+					name="description"
+					value={formData.description}
+					onChange={handleInputChange}
+				/>
+				{formErrors.description && (
+					<span className="error">{formErrors.description}</span>
+				)}
+
+				<label htmlFor="datetime">Date and Time *</label>
+				<input
+					type="datetime-local"
+					id="datetime"
+					name="datetime"
+					value={formData.datetime}
+					onChange={handleInputChange}
+				/>
+				{formErrors.datetime && (
+					<span className="error">{formErrors.datetime}</span>
+				)}
+
+				{formErrors.submit && (
+					<span className="error">{formErrors.submit}</span>
+				)}
+				{formSuccess && (
+					<span className="success">Event created successfully!</span>
+				)}
+
+				<div className="form-buttons">
+					<button type="submit">Create</button>
+					<button onClick={handleCancel}>Cancel</button>
 				</div>
 			</form>
 		</div>
